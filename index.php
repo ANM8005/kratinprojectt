fea8d45ca3cb438601aaeeb696e6baf2e2cac73a
anmol gupta <40909587+ANM8005@users.noreply.github.com>
2021-11-01 18:42:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+</head>
+<title>helping hands</title>
+</head>
+
+<body>
+<nav class="nav nav-pills nav-justified">
+  <a class="nav-item nav-link active" href="index.php">HOME</a>
+  <a class="nav-item nav-link" href="about.php">ABOUT ELDER</a>
+  <a class="nav-item nav-link" href="galary.php">GALLERY</a>
+  <a class="nav-item nav-link" href="https://www.google.com/search?q=cabs+near+you&rlz=1C1CHBF_enIN976IN976&oq=cabs+near+you+&aqs=chrome..69i57.14063j0j9&sourceid=chrome&ie=UTF-8">CABS</a>
+  <a class="nav-item nav-link " href="https://www.google.com/search?q=hospitals+in+jabalpur+mp&rlz=1C1CHBF_enIN976IN976&sxsrf=AOaemvKZkEffkveEyxnhpcuat0G6qJ3w2Q%3A1635591934563&ei=_iZ9YYXsIZ6a4-EPsr6EuAM&oq=hospitals+in+jabalpur+&gs_lcp=Cgdnd3Mtd2l6EAMYATIFCAAQywEyBQgAEMsBMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoHCCMQ6gIQJzoECCMQJzoICAAQsQMQgwE6CAgAEIAEELEDOgsIABCABBCxAxCDAToRCC4QgAQQsQMQgwEQxwEQ0QM6BggjECcQEzoGCAAQChBDOgUIABCABDoLCC4QgAQQxwEQrwE6BAguECc6CQgAEMkDEAoQQzoFCAAQkgM6CAguEIAEELEDOgcIABCABBAKOggIABDJAxDLAToFCC4QywE6CQgAELEDEAoQQzoICAAQFhAKEB5KBAhBGABQj9wTWP-fFGCOrhRoAnABeACAAYsCiAGPKJIBBDItMjOYAQCgAQGwAQrAAQE&sclient=gws-wiz">HOSPITALS</a>
+</nav>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/yy1.jpg" alt="" width="1100" height="500">
+      <div class="">
+        <h3></h3>
+        <p></p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/yy2.jpg" alt="" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/yy3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class = "my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Yoga
+    </h2>
+  </div>
+  <div class ="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="images/aabb1.jpg" class="img-fluid aboutimg">
+
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2 class = "display-4">Fact About Yoga</h2> 
+        <p class="py-3">
+        Yoga refreshes your mind and spirit. Tones your body. Keeps your internal organs and hormonal system in balance. All the more reason for people of all ages to do yoga. In fact, yoga asanas are one of the few physical exercises you can continue doing as you age. As age progresses, it is more important to focus on HOW YOU DO rather than how much you do. 
+        </p>
+        <a href="about.php"class="btn btn-success"> check more </a>
+      
+    </div>
+    
+  </div>
+  <section class = "my-5">
+  <div class="py-5">
+    <h2 class="text-center">POSES OF YOGA 
+
+    </h2>
+  </div>
+  <div class ="container fluid text-center"    >
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" style="width:400px">
+  <img class="card-img-top " src="images/AA1.jpg" calss ="margin-auto"alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">John Doe</h4>
+    <p class="card-text">Some example text.</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+     
+
+</section>
+
+
+
+  <section class = "my-5">
+  <div class="py-5">
+    <h2 class="text-center">Form</h2>
+  </div>
+  <div class ="w-50 m-auto ">
+    <form action="userinfo.php" method = "post">
+      <div class="form-group">
+        <label>username</label>
+        <input type="text" name="user" autocomplete ="off" class ="form-control">
+
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text" name="email" autocomplete ="off" class ="form-control">
+
+      </div>
+      <div class="form-group">
+        <label>Mobile</label>
+        <input type="text" name="Mobile" autocomplete ="off" class ="form-control">
+
+      </div>
+      
+      <div class="form-group">
+        <label>Comment</label>
+        <textarea class="form-control">
+
+        </textarea>
+
+      </div>
+      <button type="submit" class="btn btn-success">submit 
+        
+      </button>
+    </form>
+  </div>
+
+  </section>
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@helpinghands
+
+  </p>
+</footer>
+</body>
+
+</html>
